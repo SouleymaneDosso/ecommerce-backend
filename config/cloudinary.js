9166ad05255b2324695213f47e110be487d2d931
Dosso Souleymane <dosso070000@gmail.com>
2025-12-22 01:56:12 +0000

    mise a jour de cloudinary

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 
 // Configuration Cloudinary avec tes variables d'environnement
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_API_KEY,
 });
 
 module.exports = cloudinary;

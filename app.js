406209b2799f9d96f61964c51c5533fbd3d39010
Dosso Souleymane <dosso070000@gmail.com>
2025-12-28 15:56:17 +0000

    mise a jour de mon origin

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ mongoose
 
 // CORS
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://ecommer-numa.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"

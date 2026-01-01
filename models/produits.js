b83c3f487312a414f2455e20d555137aa402db7f
Dosso Souleymane <dosso070000@gmail.com>
2026-01-01 16:52:33 +0000

    virtual ton modia

--- models/produits.js
+++ models/produits.js
@@ -75,10 +75,11 @@ const schemaproduits = new mongoose.Schema(
 
 schemaproduits.virtual("isNew").get(function () {
   if (!this.createdAt) return false;
+
+  const created = new Date(this.createdAt); // ✅ FIX
   const days = Number(process.env.NEW_PRODUCT_DAYS) || 7;
   const now = new Date();
-  const diff = now - this.createdAt; // différence en ms
-  return diff < days * 24 * 60 * 60 * 1000;
-});
 
+  return now - created < days * 24 * 60 * 60 * 1000;
+});
 module.exports = mongoose.model("Produits", schemaproduits);

222e1f14a42efb4bd781bdc5ceb340e87caebb78
Dosso Souleymane <dosso070000@gmail.com>
2026-01-04 22:48:58 +0000

    m

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controller/userController");
-const passwordController = require("../controller/passwordController");
-
-router.post("/forgot-password", passwordController.requestPasswordReset);
-router.post("/reset-password", passwordController.resetPassword);
 
 
 router.post("/signup", userController.signup);

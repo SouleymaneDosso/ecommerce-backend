2339b4a3204fc2b71ffd65faf521df5a2adfe7ff
Dosso Souleymane <dosso070000@gmail.com>
2026-08-21 20:31:57 +0000

    m

--- models/video.js
+++ models/video.js
@@ -1,10 +1,26 @@
 const mongoose = require("mongoose");
+
 const Schema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String},
-  url: { type: String, required: true },
-  public_id: {type: String, required: true},
-  thumbnail: { type: String, required: true }
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    public_id: {
+        type: String,
+        required: true
+    },
+    thumbnail: {
+        type: String,
+        required: true
+    }
 });
 
 module.exports = mongoose.model("Video", Schema);

fcf0c611c57dfc56c5d83cb5c3554f17d18119ce
Dosso Souleymane <dosso070000@gmail.com>
2026-08-22 00:55:30 +0000

    m

--- controller/video.js
+++ controller/video.js
@@ -65,3 +65,41 @@ exports.uploadervideo = async (req, res) => {
     }
 };
 
+exports.getVideos = async (req, res) => {
+    try {
+        const videos = await Video.find();
+        return res.status(200).json({
+            message: "Vidéos récupérées avec succès.",
+            videos
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Erreur lors de la récupération des vidéos.",
+            error: error.message
+        });
+    }
+};
+
+exports.deletevideo = async (req, res)=>{
+    try{
+        const { id } = req.params;
+        const video = await Video.findById(id);
+        if (!video) {
+            return res.status(404).json({
+                message: "Vidéo non trouvée."
+            });
+        }
+        await cloudinary.uploader.destroy(video.public_id, { resource_type: "video" });
+        await Video.findByIdAndDelete(id);
+        return res.status(200).json({
+            message: "Vidéo supprimée avec succès."
+        });
+    }catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Erreur lors de la suppression de la video.",
+            error: error.message
+        });
+    }
+}
--- router/video.js
+++ router/video.js
@@ -4,4 +4,8 @@ const ajoutervideo = require("../controller/video");
 const multer = require("../config/multer");
 
 router.post("/upload", multer.array("videos"), ajoutervideo.uploadervideo);
+
+router.get("/videos", ajoutervideo.getVideos);
+router.delete("/videos/:id", ajoutervideo.deletevideo);
+
 module.exports = router; 

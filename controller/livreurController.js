54ea703abc32ad529def85248c3dbeb83900c567
Dosso Souleymane <dosso070000@gmail.com>
2026-09-02 17:41:25 +0000

    m

--- controller/livreurController.js
+++ controller/livreurController.js
@@ -495,7 +495,7 @@ exports.rechercherLivreur = async (req, res) => {
     if (commande.statusCommande !== "CONFIRMED") {
       return res.status(403).json({
         message:
-          "La livraison ne peut pas être demandée avant la confirmation de la commande par l'administrateur Veuillez patienter. Vous pouvez voir votre commande dans l'espace compte et choisir un livreur pour la commande il suffit de cliquer sur la commande.",
+          "La livraison ne peut pas être demandée avant la confirmation de la commande par l'administrateur Veuillez patienter. Vous pouvez voir votre commande dans votre espace compte. Pour choisir un livreur  il suffit de cliquer sur la commande. RDV dans votre compte client.",
         statusCommande: commande.statusCommande,
       });
     }

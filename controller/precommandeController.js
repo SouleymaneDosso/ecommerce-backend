a93b2382ac2400a6e093083209ec11a58d9a3cab
Dosso Souleymane <dosso070000@gmail.com>
2026-09-08 11:47:23 +0000

    m

--- controller/precommandeController.js
+++ controller/precommandeController.js
@@ -3,7 +3,11 @@ const Produits = require("../models/produits");
 const Video = require("../models/video");
 const User = require("../models/User");
 const mongoose = require("mongoose");
+const getNumeroDepot = () => {
+  const numero = process.env.PRECOMMANDE_NUMERO_DEPOT;
 
+  return numero ? numero.trim() : "";
+};
 
 /* =====================================================
    MODÈLES À VENIR
@@ -77,13 +81,7 @@ exports.getModelesPrecommande = async (req, res) => {
 
 exports.getInformationsDepot = async (req, res) => {
   try {
-    const numeroDepot =  () => {
-  const numero = process.env.PRECOMMANDE_NUMERO_DEPOT;
-
-  console.log("NUMERO DEPOT ENV :", numero);
-
-  return numero ? numero.trim() : "";
-};
+    const numeroDepot = getNumeroDepot();
 
     if (!numeroDepot) {
       return res.status(500).json({
@@ -274,8 +272,7 @@ exports.creerPrecommande = async (req, res) => {
           }
         }
       } else {
-
-      /* =========================
+        /* =========================
          SANS COULEUR
       ========================= */
         if (typeof variationTaille === "number") {

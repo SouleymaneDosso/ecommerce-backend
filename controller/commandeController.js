94f8d343b857fe17477eccac8a6b84f3e9045fe5
Dosso Souleymane <dosso070000@gmail.com>
2026-09-16 00:30:21 +0000

    m

--- controller/commandeController.js
+++ controller/commandeController.js
@@ -560,15 +560,26 @@ const rejeterPaiementAdmin = async (req, res) => {
     }
 
     // ---------- Mettre à jour le statut global ----------
-    if (commande.modePaiement !== "cod") {
-      if (commande.paiements.every((p) => p.status === "PAID")) {
-        commande.statusCommande = "PAID";
-      } else if (commande.paiements.some((p) => p.status === "PENDING")) {
-        commande.statusCommande = "PARTIALLY_PAID";
-      } else {
-        commande.statusCommande = "PENDING";
-      }
-    }
+
+if (commande.modePaiement !== "cod") {
+  const tousLesPaiementsPayes = commande.paiements.every(
+    (p) => p.status === "PAID"
+  );
+
+  const paiementEnAttente = commande.paiements.some(
+    (p) => p.status === "PENDING"
+  );
+
+  if (tousLesPaiementsPayes) {
+    commande.statusCommande = "CONFIRMED";
+  } else if (paiementEnAttente) {
+    commande.statusCommande = "PARTIALLY_PAID";
+  } else {
+    commande.statusCommande = "PENDING";
+  }
+}
+
+
 
     await commande.save({ session });
     const clientUser = await User.findById(commande.client.userId);
